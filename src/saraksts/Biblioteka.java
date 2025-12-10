b34907e105c082e8553766ef8556fd3f1d54cf33
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-12-10 08:43:12 +0200

    Jauna main klase Biblioteka

---
+++ src/saraksts/Biblioteka.java
@@ -0,0 +1,22 @@
+package saraksts;
+
+import java.util.LinkedList;
+
+public class Biblioteka {
+
+	public static void main(String[] args) {
+		String nosaukums, autors, izdevnieciba, izvelne;
+		int id = 1, skaits, lppSk, indekss;
+		double cena;
+		LinkedList<gramata> plaukts = new LinkedList<>();
+		String[] darbibas = {"Pievienot grāmatu",
+				"Noņemt grāmatu", "Apskatīt grāmatu",
+				"Iznomāt grāmatu", "Apturēt"};
+		
+		do {
+			
+		} while();
+
+	}
+
+}
