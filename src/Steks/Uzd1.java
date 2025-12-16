fe13d9da8c8944b5c5f340a2ae650165cc289c1e
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-12-16 08:46:38 +0200

    1 uzdevums 85% pabeigts

--- src/Steks/Uzd1.java
+++ src/Steks/Uzd1.java
@@ -20,25 +20,75 @@ public class Uzd1 {
 					JOptionPane.QUESTION_MESSAGE, null,
 					darbibas, darbibas[0]);
 			
+			if(izvele == null)
+				izvele = "Apturēt";
+			
 		switch(izvele) {
 		case "Ievadīt vārdu":
-			
+			if(mansSteks.isEmpty()) {
+				do {
+					vards = JOptionPane.showInputDialog("Ievadi vārdu");
+				} while(vards.contains(" "));
+				
+				for(int i=0; i<vards.length(); i++) {
+					mansSteks.push(Character.toString(vards.charAt(i)));
+				}
+				
+			} else
+				JOptionPane.showMessageDialog(null,
+						"Stekā jau atrodas vārds!", "Brīdinājums",
+						JOptionPane.WARNING_MESSAGE);
 			break;
 			
 		case "Noteikt vārda garumu":
+			if(!mansSteks.isEmpty())
+				JOptionPane.showMessageDialog(null,
+						"Stekā esošā vārda garums: "+mansSteks.size(),
+						"Paziņojuma", JOptionPane.INFORMATION_MESSAGE);
+			
+			else
+				JOptionPane.showMessageDialog(null,
+						"Steks ir tukšs!",
+						"Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 			
 			break;
 			
 		case "Apskatīt pēdējo burtu":
-	
+			if(!mansSteks.isEmpty())
+				JOptionPane.showMessageDialog(null,
+						"Stekā esošā vārda pēdējais burts: "+mansSteks.peek(),
+						"Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+			
+			else
+				JOptionPane.showMessageDialog(null,
+						"Steks ir tukšs!",
+						"Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+			
 			break;
 	
 		case "Apgriezt vārdu":
-	
+	if(!mansSteks.isEmpty()) {
+		
+		vards = "";
+		int garums = mansSteks.size();
+		for(int i=0; i<garums; i++) {
+			vards += mansSteks.pop();
+		}
+		
+		JOptionPane.showMessageDialog(null,
+				"Apgrieztais vārds: "+vards, "Paziņojums",
+				JOptionPane.INFORMATION_MESSAGE);
+		
+	} else
+		JOptionPane.showMessageDialog(null,
+				"Steks ir tukšs!",
+				"Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 			break;
 	
 		case "Apturēt":
-	
+			JOptionPane.showMessageDialog(null,
+					"Programma apturēta!", "Informācija",
+					JOptionPane.INFORMATION_MESSAGE);
 			break;
 		}
 		} while(!izvele.equals("Apturēt"));
