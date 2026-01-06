7a6b7f5c13dd607d7fe7c5479878950581478d30
LiteDuck1986 <emils2007smirnovs@gmail.com>
2026-01-06 08:34:09 +0200

    Queue/rinda programmas piemērs, sākuma posms

---
+++ src/Rinda_Queue/Pasts.java
@@ -0,0 +1,20 @@
+package Rinda_Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Pasts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String izvelne, cilveks = null;
+		int kartasNr;
+		Queue<String> rinda = new LinkedList<>();
+		
+		String[] darbibas = {"Nostāties rindā", "Apskatīt pirmo rindā stāvošo", "Noteikt rindas garumu", "Cik pirms manis?",
+			"Aplūkot visus rindā stāvošos", "Apkalpot pirmo", "Apturēt"};
+		
+		
+	}
+
+}
